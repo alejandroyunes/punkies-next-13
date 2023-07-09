dca188dc1f1ebaf0557172b808309fb660a7d6ad
Alejandro Cano <alejandro.cano@grupor5.com>
2023-07-08 21:23:41 -0500

    adding new nextjs with tailwind

---
+++ app/musica/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import DesktopHeader  from '@/src/components/navigation/desktop/DesktopHeader'
+
+export default function Home() {
+  return (
+    <>
+    {/* <DesktopHeader/> */}
+    <h1>hello from music</h1>
+
+    </>
+  )
+}
---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function notFound() {
+  return (
+    <>
+    <h1>Error page</h1>
+
+    </>
+  )
+}
